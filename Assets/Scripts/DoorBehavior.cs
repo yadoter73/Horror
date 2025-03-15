c4b1c174b29dab6a420dfffd3488472e452b045e
yadoter73 <tomanovpl@gmail.com>
2025-03-15 18:16:55 +0300

    jjj
    jjj

--- Assets/Scripts/DoorBehavior.cs
+++ Assets/Scripts/DoorBehavior.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, Interactable
 {
-    public Animator _animator;
+    public Animator animator;
     public bool IsOpen;
     void Start()
     {
+        animator = GetComponent<Animator>();
         if (IsOpen)
         {
-            _animator.SetBool("IsOpen", true);
+            animator.SetBool("IsOpen", true);
         }
     }
     public string GetDescription()
     {
-        if (IsOpen) return "Press E to close the door";
+        if (IsOpen)
+        {
+            return "Press E to close the door";
+        }
         return "Press E to open the door";
     }
     public void Interact()
     {
+        //IsOpen = !IsOpen
         IsOpen = !IsOpen;
         if (IsOpen)
         {
-            _animator.SetBool("IsOpen", true);
+            animator.SetBool("IsOpen", true);
         }
         else
         {
-            _animator.SetBool("IsOpen", false);
+            animator.SetBool("IsOpen", false);
         }
 
     }
